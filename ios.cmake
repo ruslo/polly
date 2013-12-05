28a42f86420df63308462284d1a265b6bbd82991
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-12-05 23:17:06 +0400

    ios toolchain: clear environment variables setted by Xcode

--- ios.cmake
+++ ios.cmake
@@ -7,15 +7,7 @@ else()
   set(POLLY_IOS_CMAKE 1)
 endif()
 
-# Error while building using 'ExternalProject_Add':
-
-#CMake Error at /.../share/cmake/Modules/Platform/Darwin.cmake:211 (message):
-#  CMAKE_OSX_DEPLOYMENT_TARGET is '10.9' but CMAKE_OSX_SYSROOT:
-#   "iphoneos"
-#  is not set to a MacOSX SDK with a recognized version.  Either set
-#  CMAKE_OSX_SYSROOT to a valid SDK or set CMAKE_OSX_DEPLOYMENT_TARGET to
-#  empty.
-set(ENV{CMAKE_OSX_DEPLOYMENT_TARGET} "")
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
 
 set(POLLY_TOOLCHAIN_NAME "iOS Universal (iphoneos + iphonesimulator)")
 set(POLLY_TOOLCHAIN_TAG "ios")
