13704478c9bf4e3a1353e7117701e3c3f6c8b374
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-12-05 18:41:44 +0400

    Add gcc 4.8 and clang 3.2 toolchains

---
+++ gcc48.cmake
@@ -0,0 +1,19 @@
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_GCC48_CMAKE)
+  return()
+else()
+  set(POLLY_GCC48_CMAKE 1)
+endif()
+
+set(
+    POLLY_TOOLCHAIN_NAME
+    "gcc 4.8 / c++11 support"
+)
+set(POLLY_TOOLCHAIN_TAG "gcc48")
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc48.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake")
