649538fded37756805e64f09e396c55a967a73f8
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-01-13 23:45:12 +0400

    Add toolchain: sanitize_address (clang address sanitizer)

---
+++ flags/sanitize_address.cmake
@@ -0,0 +1,17 @@
+# Copyright (c) 2014, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_FLAGS_SANITIZE_ADDRESS_CMAKE_)
+  return()
+else()
+  set(POLLY_FLAGS_SANITIZE_ADDRESS_CMAKE_ 1)
+endif()
+
+set(
+    CMAKE_CXX_FLAGS
+    "${CMAKE_CXX_FLAGS} -fsanitize=address"
+    CACHE
+    STRING
+    "C++ compiler flags"
+    FORCE
+)
---
+++ sanitize_address.cmake
@@ -0,0 +1,21 @@
+# Copyright (c) 2014, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_SANITIZE_ADDRESS_CMAKE_)
+  return()
+else()
+  set(POLLY_SANITIZE_ADDRESS_CMAKE_ 1)
+endif()
+
+set(
+    POLLY_TOOLCHAIN_NAME
+    "Clang address sanitizer / c++11 support"
+)
+set(POLLY_TOOLCHAIN_TAG "sanitize_address")
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/flags/sanitize_address.cmake")
