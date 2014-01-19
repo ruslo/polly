18e1bb66346060987985a4c3175551973907b8f8
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-01-19 18:32:34 +0400

    analyze: fix mac os

--- scripts/clang-analyze.sh
+++ scripts/clang-analyze.sh
@@ -9,17 +9,19 @@ for x in "$@";
 do
   if [ "${x}" == "-c" ];
   then
-    temp="`mktemp`"
+    temp_out="`mktemp /tmp/polly-clang-analyze.out.XXXXX`"
+    temp_bin="`mktemp /tmp/polly-clang-analyze.bin.XXXXX`"
 
     # analyze
-    clang++ --analyze "$@" 2> "${temp}"
+    clang++ --analyze "$@" -o "${temp_bin}" 2> "${temp_out}"
 
     RESULT=0
     [ "$?" == 0 ] || RESULT=1
-    [ -s "${temp}" ] && RESULT=1
+    [ -s "${temp_out}" ] && RESULT=1
 
-    cat "${temp}";
-    rm -f "${temp}"
+    cat "${temp_out}";
+    rm -f "${temp_out}"
+    rm -f "${temp_bin}"
 
     if [ "${RESULT}" == "1" ];
     then
