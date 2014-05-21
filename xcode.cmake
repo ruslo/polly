2a262f995bb1e8e858384085887e63308efc48a4
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-05-21 19:59:18 +0400

    Add xcode toolchain

---
+++ xcode.cmake
@@ -0,0 +1,44 @@
+# Copyright (c) 2014, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_XCODE_CMAKE_)
+  return()
+else()
+  set(POLLY_XCODE_CMAKE_ 1)
+endif()
+
+set(
+    POLLY_TOOLCHAIN_NAME
+    "Xcode / LLVM Standard C++ Library (libc++) / c++11 support"
+)
+set(POLLY_TOOLCHAIN_TAG "xcode")
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+if(NOT XCODE_VERSION)
+  polly_fatal_error("This toolchain is available only for Xcode")
+endif()
+
+set(HUNTER_CMAKE_GENERATOR Xcode)
+
+execute_process(
+    COMMAND
+    xcrun --find clang
+    OUTPUT_VARIABLE
+    CMAKE_C_COMPILER
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+execute_process(
+    COMMAND
+    xcrun --find clang++
+    OUTPUT_VARIABLE
+    CMAKE_CXX_COMPILER
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+set(CMAKE_C_COMPILER ${CMAKE_C_COMPILER} CACHE STRING "C compiler" FORCE)
+set(CMAKE_CXX_COMPILER ${CMAKE_CXX_COMPILER} CACHE STRING "C++ compiler" FORCE)
+
+include("${CMAKE_CURRENT_LIST_DIR}/library/std/libcxx.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake")
