1be397ff29d1d8a74141eb527d9c1cb35d959db3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-07-03 18:32:51 +0400

    Remove debug print

--- bin/detail/win32.py
+++ bin/detail/win32.py
@@ -12,10 +12,8 @@ def get_short_path_name(long_name):
   """
   output_buf_size = 0
   while True:
-    print("begin output_buf_size: {}".format(output_buf_size))
     output_buf = ctypes.create_unicode_buffer(output_buf_size)
     needed = _GetShortPathNameW(long_name, output_buf, output_buf_size)
-    print("Needed: {}".format(needed))
     if output_buf_size >= needed:
       return output_buf.value
     else:
