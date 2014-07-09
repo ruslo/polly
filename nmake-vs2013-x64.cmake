929ce998c7170868c4d444849626fb680c3c59c9
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-07-09 23:09:25 +0400

    NMake: add hunter variables

--- nmake-vs2013-x64.cmake
+++ nmake-vs2013-x64.cmake
@@ -14,3 +14,6 @@ set(
 set(POLLY_TOOLCHAIN_TAG "nmake-vs2013-x64")
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+set(HUNTER_MSVC_ARCH "amd64")
+set(HUNTER_MSVC_VCVARSALL "$ENV{VS120COMNTOOLS}/../../VC/vcvarsall.bat")
+set(HUNTER_CMAKE_GENERATOR "NMake Makefiles")
