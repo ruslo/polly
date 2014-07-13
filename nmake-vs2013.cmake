a832b8366f922bc2f3b2a54533f77aef2f866b56
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-07-13 20:18:15 +0400

    Add nmake x86 toolchain

--- nmake-vs2013.cmake
+++ nmake-vs2013.cmake
@@ -1,17 +1,17 @@
 # Copyright (c) 2014, Ruslan Baratov
 # All rights reserved.
 
-if(DEFINED POLLY_NMAKE_VS2013_X64_CMAKE_)
+if(DEFINED POLLY_NMAKE_VS2013_CMAKE_)
   return()
 else()
-  set(POLLY_NMAKE_VS2013_X64_CMAKE_ 1)
+  set(POLLY_NMAKE_VS2013_CMAKE_ 1)
 endif()
 
-set(POLLY_TOOLCHAIN_NAME "NMake / Visual Studio 2013 / x64")
-set(POLLY_TOOLCHAIN_TAG "nmake-vs2013-x64")
+set(POLLY_TOOLCHAIN_NAME "NMake / Visual Studio 2013 / x86")
+set(POLLY_TOOLCHAIN_TAG "nmake-vs2013")
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/msvc-2013.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/arch/msvc-x64.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/arch/msvc-x86.cmake")
 
 set(HUNTER_CMAKE_GENERATOR "NMake Makefiles")
