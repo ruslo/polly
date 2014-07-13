727e6bda10aa7ac4449a88b2a04f13a467d9757f
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-07-13 20:17:03 +0400

    Visual Studio toolchains: add HUNTER_MSVC_* variables

--- vs-12-2013-win64.cmake
+++ vs-12-2013-win64.cmake
@@ -1,15 +1,15 @@
 # Copyright (c) 2014, Ruslan Baratov
 # All rights reserved.
 
-if(DEFINED POLLY_VS_12_2013_WIN64_CMAKE)
+if(DEFINED POLLY_VS_12_2013_WIN64_CMAKE_)
   return()
 else()
-  set(POLLY_VS_12_2013_WIN64_CMAKE 1)
+  set(POLLY_VS_12_2013_WIN64_CMAKE_ 1)
 endif()
 
-# Compatible with default Hunter settings:
-# * https://github.com/ruslo/hunter/blob/master/cmake/Hunter
 set(POLLY_TOOLCHAIN_NAME "Visual Studio 12 2013 Win64")
 set(POLLY_TOOLCHAIN_TAG "vs-12-2013-win64")
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/msvc-2013.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/arch/msvc-x64.cmake")
