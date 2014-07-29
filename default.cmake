0c5c3898df074ad3385f0260ed131836608f8fc0
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-07-29 09:23:40 +0400

    Fix default tag name

--- default.cmake
+++ default.cmake
@@ -9,6 +9,6 @@ endif()
 
 # Don't use polly_init (no generator expected)
 set(POLLY_TOOLCHAIN_NAME "Default")
-set(POLLY_TOOLCHAIN_TAG "Default")
+set(POLLY_TOOLCHAIN_TAG "default")
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
