d203cb84e7302c8a1ef3d8bc2d862b950cf74697
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-08-23 18:16:50 +0400

    build.py: toolchains from table

--- bin/build.py
+++ bin/build.py
@@ -14,34 +14,41 @@ import platform
 
 import detail.toolchain_name
 
+class Toolchain:
+  def __init__(self, name, generator):
+    self.name = name
+    self.generator = generator
+
+toolchain_table = [
+    Toolchain('analyze', 'Unix Makefiles'),
+    Toolchain('clang-lto', 'Unix Makefiles'),
+    Toolchain('clang_libstdcxx', 'Unix Makefiles'),
+    Toolchain('cygwin', 'Unix Makefiles'),
+    Toolchain('default', ''),
+    Toolchain('gcc', 'Unix Makefiles'),
+    Toolchain('gcc48', 'Unix Makefiles'),
+    Toolchain('ios', 'Xcode'),
+    Toolchain('ios-nocodesign', 'Xcode'),
+    Toolchain('libcxx', 'Unix Makefiles'),
+    Toolchain('mingw', 'MinGW Makefiles'),
+    Toolchain('nmake-vs-12-2013', 'NMake Makefiles'),
+    Toolchain('nmake-vs-12-2013-win64', 'NMake Makefiles'),
+    Toolchain('sanitize_address', 'Unix Makefiles'),
+    Toolchain('sanitize_leak', 'Unix Makefiles'),
+    Toolchain('sanitize_memory', 'Unix Makefiles'),
+    Toolchain('sanitize_thread', 'Unix Makefiles'),
+    Toolchain('vs-12-2013', 'Visual Studio 12 2013'),
+    Toolchain('vs-12-2013-win64', 'Visual Studio 12 2013 Win64'),
+    Toolchain('xcode', 'Xcode'),
+]
+
 assert(sys.version_info.major == 3)
 assert(sys.version_info.minor >= 2) # Current cygwin version is 3.2.3
 
 parser = argparse.ArgumentParser(description="Script for building")
 parser.add_argument(
     '--toolchain',
-    choices=[
-        'analyze',
-        'clang-lto',
-        'clang_libstdcxx',
-        'cygwin',
-        'default',
-        'gcc',
-        'gcc48',
-        'ios',
-        'ios-nocodesign',
-        'libcxx',
-        'mingw',
-        'nmake-vs-12-2013',
-        'nmake-vs-12-2013-win64',
-        'sanitize_address',
-        'sanitize_leak',
-        'sanitize_memory',
-        'sanitize_thread',
-        'vs-12-2013',
-        'vs-12-2013-win64',
-        'xcode'
-    ],
+    choices=[x.name for x in toolchain_table],
     help="CMake generator/toolchain",
 )
 
