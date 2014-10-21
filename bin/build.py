1fd23d3aa90c7a0b64b618bcfb3c7ecab0ed5e9d
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-10-21 17:41:22 +0400

    build.py: detect directory by self location

--- bin/build.py
+++ bin/build.py
@@ -87,9 +87,8 @@ polly_toolchain = detail.toolchain_name.get(args.toolchain)
 toolchain_entry = detail.toolchain_table.get_by_name(polly_toolchain)
 cpack_generator = detail.cpack_generator.get(args.pack)
 
-polly_root = os.getenv("POLLY_ROOT")
-if not polly_root:
-  sys.exit("Environment variable `POLLY_ROOT` is empty")
+polly_root = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+polly_root = os.path.realpath(polly_root)
 
 """Build directory tag"""
 if args.config and not toolchain_entry.multiconfig:
