c5131fdaa0d48461a73d0713c99db5157cff8c63
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-11 14:40:35 +0200

    Full path to compiler

--- compiler/gcc.cmake
+++ compiler/gcc.cmake
@@ -7,5 +7,31 @@ else()
   set(POLLY_COMPILER_GCC_CMAKE 1)
 endif()
 
-set(CMAKE_C_COMPILER gcc CACHE STRING "C compiler" FORCE)
-set(CMAKE_CXX_COMPILER g++ CACHE STRING "C++ compiler" FORCE)
+find_program(CMAKE_C_COMPILER gcc)
+find_program(CMAKE_CXX_COMPILER g++)
+
+if(NOT CMAKE_C_COMPILER)
+  polly_fatal_error("gcc not found")
+endif()
+
+if(NOT CMAKE_CXX_COMPILER)
+  polly_fatal_error("g++ not found")
+endif()
+
+set(
+    CMAKE_C_COMPILER
+    "${CMAKE_C_COMPILER}"
+    CACHE
+    STRING
+    "C compiler"
+    FORCE
+)
+
+set(
+    CMAKE_CXX_COMPILER
+    "${CMAKE_CXX_COMPILER}"
+    CACHE
+    STRING
+    "C++ compiler"
+    FORCE
+)
