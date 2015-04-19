ce15e89c094c80545e1c681acc5eae871faf9f41
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-19 20:24:42 +0200

    Add flags/msvc-parallel: /MP

---
+++ flags/msvc-parallel.cmake
@@ -0,0 +1,13 @@
+# Copyright (c) 2015, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_FLAGS_MSVC_PARALLEL_CMAKE_)
+  return()
+else()
+  set(POLLY_FLAGS_MSVC_PARALLEL_CMAKE_ 1)
+endif()
+
+include(polly_add_cache_flag)
+
+polly_add_cache_flag(CMAKE_CXX_FLAGS "/MP")
+polly_add_cache_flag(CMAKE_C_FLAGS "/MP")
