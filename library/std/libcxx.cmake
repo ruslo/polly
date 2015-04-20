e66a3c8ceeacfcdf9f35a2787ea3f6fb8c8122ec
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-04-20 16:33:27 +0200

    Flags "-stdlib=libc++" for linker need only for Xcode < 6

--- library/std/libcxx.cmake
+++ library/std/libcxx.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2013, Ruslan Baratov
+# Copyright (c) 2013, 2015, Ruslan Baratov
 # All rights reserved.
 
 if(DEFINED POLLY_LIBRARY_STD_LIBCXX_CMAKE)
@@ -10,5 +10,10 @@ endif()
 include(polly_add_cache_flag)
 
 polly_add_cache_flag(CMAKE_CXX_FLAGS "-stdlib=libc++")
-polly_add_cache_flag(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++")
-polly_add_cache_flag(CMAKE_SHARED_LINKER_FLAGS "-stdlib=libc++")
+
+if(XCODE)
+  if(XCODE_VERSION VERSION_LESS 6)
+    polly_add_cache_flag(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++")
+    polly_add_cache_flag(CMAKE_SHARED_LINKER_FLAGS "-stdlib=libc++")
+  endif()
+endif()
