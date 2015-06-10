e12ba80cfe7e351ced93a8da0667c362d756ab11
David Hirvonen <dhirvonen@elucideye.com>
2015-06-10 10:25:39 -0400

    Updated include guards to reflect NEON ABI

--- android-ndk-r10e-api-21-armeabi-v7a-neon.cmake
+++ android-ndk-r10e-api-21-armeabi-v7a-neon.cmake
@@ -2,10 +2,10 @@
 # Copyright (c) 2015, David Hirvonen
 # All rights reserved.
 
-if(DEFINED POLLY_ANDROID_NDK_R10E_API_21_ARMEABI_V7A_CMAKE_)
+if(DEFINED POLLY_ANDROID_NDK_R10E_API_21_ARMEABI_V7A_NEON_CMAKE_)
   return()
 else()
-  set(POLLY_ANDROID_NDK_R10E_API_21_ARMEABI_V7A_CMAKE_ 1)
+  set(POLLY_ANDROID_NDK_R10E_API_21_ARMEABI_V7A_NEON_CMAKE_ 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
