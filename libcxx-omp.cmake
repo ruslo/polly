98ffd76c2a06fcb5d2d410a196c7d82398323b57
David Hirvonen <dhirvonen@elucideye.com>
2015-06-10 10:22:38 -0400

    query replace: clang => clang-omp

--- libcxx-omp.cmake
+++ libcxx-omp.cmake
@@ -1,16 +1,16 @@
 # Copyright (c) 2013, Ruslan Baratov
 # All rights reserved.
 
-if(DEFINED POLLY_CLANG_LIBCXX_CXX11_CMAKE)
+if(DEFINED POLLY_CLANG_OMP_LIBCXX_CXX11_CMAKE)
   return()
 else()
-  set(POLLY_CLANG_LIBCXX_CXX11_CMAKE 1)
+  set(POLLY_CLANG_OMP_LIBCXX_CXX11_CMAKE 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
 polly_init(
-    "clang / LLVM Standard C++ Library (libc++) / c++11 support"
+    "clang-omp / LLVM Standard C++ Library (libc++) / c++11 support"
     "Unix Makefiles"
 )
 
