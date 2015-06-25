b208b0d359d24fba8c75c2a803f2326da73bf48d
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-06-25 15:26:43 +0300

    build.py create_framework: exclude 'cmake' dir from search

--- bin/detail/create_framework.py
+++ bin/detail/create_framework.py
@@ -19,6 +19,7 @@ def get_framework_name(lib_name):
 def run(install_dir, framework_dir, logging):
   libs_path = os.path.join(install_dir, 'lib')
   libs = glob.glob(os.path.join(libs_path, '*'))
+  libs.remove(os.path.join(libs_path, 'cmake'))
 
   if len(libs) == 0:
     sys.exit('No libs found in directory: {}'.format(libs_path))
