ad2f1f128a82e5b52aca04a1a5b3642bce0a3e53
David Hirvonen <dhirvonen@elucideye.com>
2015-07-03 19:53:56 -0400

    Added 8.2 and 8.4 iOS SDK toolchains with libc++

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -111,7 +111,9 @@ if platform.system() == 'Linux':
 
 if platform.system() == 'Darwin':
   toolchain_table += [
+      Toolchain('ios-8-4-libcxx', 'Xcode', ios_version='8.4'),
       Toolchain('ios-8-2', 'Xcode', ios_version='8.2'),
+      Toolchain('ios-8-2-libcxx', 'Xcode', ios_version='8.2'),
       Toolchain('ios-8-2-i386-arm64', 'Xcode', ios_version='8.2'),
       Toolchain('ios-8-2-arm64', 'Xcode', ios_version='8.2'),
       Toolchain('ios-8-2-arm64-hid', 'Xcode', ios_version='8.2'),
