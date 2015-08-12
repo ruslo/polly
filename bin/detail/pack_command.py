8f97fa4ddc3350563db44e335f7eaf5c02801a67
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-08-12 14:36:10 +0300

    build.py: use full path to CPack on Windows
    Chocolatey pack command has the same name (cpack.exe). To avoid
    confusion use full path to the CMake's CPack.

--- bin/detail/pack_command.py
+++ bin/detail/pack_command.py
@@ -1,10 +1,21 @@
 # Copyright (c) 2014, Ruslan Baratov
 # All rights reserved.
 
+import os
+import subprocess
+
 import detail.call
 
 def run(config, logging, cpack_generator):
   pack_command = ['cpack']
+  if os.name == 'nt':
+    # use full path to cpack since Chocolatey pack command has the same name
+    cmake_list = subprocess.check_output(
+        ['where', 'cmake'], universal_newlines=True
+    )
+    cmake_path = cmake_list.split('\n')[0]
+    cpack_path = os.path.join(os.path.dirname(cmake_path), 'cpack')
+    pack_command = [cpack_path]
   if config:
     pack_command.append('-C')
     pack_command.append(config)
