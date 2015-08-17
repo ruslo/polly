f76d51ae362cc4ddfb7f053524d6a67876c0d33a
David Hirvonen <dhirvonen@elucideye.com>
2015-08-17 12:21:47 -0400

    Added more x86 and x86-64 android toolchains; fix dictionary sort

---
+++ android-ndk-r10e-api-16-x86-hid.cmake
@@ -0,0 +1,34 @@
+# Copyright (c) 2015, Ruslan Baratov, David Hirvonen
+# All rights reserved.
+
+if(DEFINED POLLY_ANDROID_NDK_R10E_API_16_X86_HID_CMAKE_)
+  return()
+else()
+  set(POLLY_ANDROID_NDK_R10E_API_16_X86_HID_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+set(ANDROID_FORCE_ARM_BUILD "OFF")
+set(ANDROID_NDK_VERSION "r10e")
+set(ANDROID_NATIVE_API_LEVEL "16")
+set(ANDROID_ABI "x86")
+set(ANDROID_TOOLCHAIN_NAME "x86-4.9")
+
+polly_init(
+    "Android NDK ${ANDROID_NDK_VERSION} / \
+API ${ANDROID_NATIVE_API_LEVEL} / ${ANDROID_ABI} / \
+hidden visibility / \
+c++11 support"
+    "Unix Makefiles"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake") # before toolchain!
+
+include("${CMAKE_CURRENT_LIST_DIR}/flags/hidden.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/os/android.cmake")
--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -45,9 +45,12 @@ class Toolchain:
 
 toolchain_table = [
     Toolchain('default', ''),
+    Toolchain('android-ndk-r10e-api-8-armeabi-v7a', 'Unix Makefiles'),
     Toolchain('android-ndk-r10e-api-16-armeabi-v7a-neon', 'Unix Makefiles'),
     Toolchain('android-ndk-r10e-api-16-armeabi-v7a-neon-clang-35', 'Unix Makefiles'),
     Toolchain('android-ndk-r10e-api-16-armeabi-v7a-neon-clang-35-hid', 'Unix Makefiles'),
+    Toolchain('android-ndk-r10e-api-16-x86', 'Unix Makefiles'),
+    Toolchain('android-ndk-r10e-api-16-x86-hid', 'Unix Makefiles'),
     Toolchain('android-ndk-r10e-api-19-armeabi-v7a-neon', 'Unix Makefiles'),
     Toolchain('android-ndk-r10e-api-21-armeabi-v7a', 'Unix Makefiles'),
     Toolchain('android-ndk-r10e-api-21-armeabi-v7a-neon', 'Unix Makefiles'),
@@ -57,7 +60,8 @@ toolchain_table = [
     Toolchain('android-ndk-r10e-api-21-arm64-v8a-gcc-49-hid', 'Unix Makefiles'),
     Toolchain('android-ndk-r10e-api-21-arm64-v8a-clang-35', 'Unix Makefiles'),
     Toolchain('android-ndk-r10e-api-21-x86', 'Unix Makefiles'),
-    Toolchain('android-ndk-r10e-api-8-armeabi-v7a', 'Unix Makefiles'),
+    Toolchain('android-ndk-r10e-api-21-x86-64', 'Unix Makefiles'),
+    Toolchain('android-ndk-r10e-api-21-x86-64-hid', 'Unix Makefiles'),
     Toolchain('raspberrypi2-cxx11', 'Unix Makefiles')
 ]
 
