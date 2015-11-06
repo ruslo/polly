66a965b72f138ceedbe0817fbdf0edfefcefdd26
David Hirvonen <dhirvonen@elucideye.com>
2015-11-06 17:56:14 -0500

    Revert "Added ios-9-1-arm64 toolchain"
    This reverts commit a6655211b2880fa31ba51eefc2930ec6b6092886.

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -131,7 +131,6 @@ if platform.system() == 'Linux':
 if platform.system() == 'Darwin':
   toolchain_table += [
       Toolchain('ios-9-1-armv7', 'Xcode', ios_version='9.1'),
-      Toolchain('ios-9-1-arm64', 'Xcode', ios_version='9.1'),
       Toolchain('ios-9-1-dep-7-0-armv7', 'Xcode', ios_version='9.1'),
       Toolchain('ios-9-0', 'Xcode', ios_version='9.0'),
       Toolchain('ios-9-0-armv7', 'Xcode', ios_version='9.0'),
