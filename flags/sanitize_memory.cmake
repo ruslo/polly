dc4a3ac650604260833cdee4ef707a56f5176140
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-11-25 13:15:28 +0700

    flags/sanitize_memory: update C flags too

--- flags/sanitize_memory.cmake
+++ flags/sanitize_memory.cmake
@@ -8,6 +8,11 @@ else()
 endif()
 
 include(polly_add_cache_flag)
+
 polly_add_cache_flag(CMAKE_CXX_FLAGS "-fsanitize=memory")
 polly_add_cache_flag(CMAKE_CXX_FLAGS "-fsanitize-memory-track-origins")
 polly_add_cache_flag(CMAKE_CXX_FLAGS "-g")
+
+polly_add_cache_flag(CMAKE_C_FLAGS "-fsanitize=memory")
+polly_add_cache_flag(CMAKE_C_FLAGS "-fsanitize-memory-track-origins")
+polly_add_cache_flag(CMAKE_C_FLAGS "-g")
