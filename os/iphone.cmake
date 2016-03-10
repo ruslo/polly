8acfed7d60130aea21d82ac160d53bdd8b16a264
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-03-10 15:15:39 +0700

    Minimum version for iOS is 3.5

--- os/iphone.cmake
+++ os/iphone.cmake
@@ -20,6 +20,12 @@ if(XCODE_VERSION VERSION_LESS "5.0.0")
   polly_fatal_error("Works since Xcode 5.0.0 (current ver: ${XCODE_VERSION})")
 endif()
 
+if(CMAKE_VERSION VERSION_LESS "3.5")
+  polly_fatal_error(
+      "CMake minimum required version for iOS is 3.5 (current ver: ${CMAKE_VERSION})"
+  )
+endif()
+
 string(COMPARE EQUAL "$ENV{DEVELOPER_DIR}" "" _is_empty)
 if(NOT _is_empty)
   polly_status_debug("Developer root (env): $ENV{DEVELOPER_DIR}")
