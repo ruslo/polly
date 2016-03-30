95d74942c386f84dcd8799fc74a0fa16a2538476
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-03-31 03:07:05 +0700

    Use pruned Android archives for Travis
    [skip ci]

--- bin/install-ci-dependencies.py
+++ bin/install-ci-dependencies.py
@@ -121,7 +121,10 @@ if not os.path.exists(ci_dir):
   os.mkdir(ci_dir)
 
 cmake_archive_local = os.path.join(ci_dir, 'cmake-version.archive')
-android_archive_local = os.path.join(ci_dir, 'android.bin')
+if os.getenv('TRAVIS'):
+  android_archive_local = os.path.join(ci_dir, 'android.tar.gz')
+else:
+  android_archive_local = os.path.join(ci_dir, 'android.bin')
 ninja_archive_local = os.path.join(ci_dir, 'ninja.zip')
 
 expected_files = [
@@ -167,23 +170,21 @@ elif platform.system() == 'Windows':
 else:
   sys.exit('Unknown system: {}'.format(platform.system()))
 
-if is_android:
+def get_android_url():
+  if os.getenv('TRAVIS'):
+    if os.getenv('TOOLCHAIN') == 'android-ndk-r10e-api-19-armeabi-v7a-neon':
+      if platform.system() == 'Linux':
+        return 'https://github.com/hunter-packages/android-ndk/releases/download/v1.0.0/android-ndk-r10e-arm-linux-androideabi-4.9-gnu-libstdc.-4.9-armeabi-v7a-android-19-arch-arm-Linux.tar.gz', '847177799b0fe4f7480f910bbf1815c3e3fed0da'
   if platform.system() == 'Darwin':
-    android = FileToDownload(
-        'http://dl.google.com/android/ndk/android-ndk-r10e-darwin-x86_64.bin',
-        'b57c2b9213251180dcab794352bfc9a241bf2557',
-        android_archive_local,
-        ci_dir
-    )
+    return 'http://dl.google.com/android/ndk/android-ndk-r10e-darwin-x86_64.bin', 'b57c2b9213251180dcab794352bfc9a241bf2557',
   elif platform.system() == 'Linux':
-    android = FileToDownload(
-        'http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin',
-        'c685e5f106f8daa9b5449d0a4f21ee8c0afcb2f6',
-        android_archive_local,
-        ci_dir
-    )
+    return 'http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin', 'c685e5f106f8daa9b5449d0a4f21ee8c0afcb2f6',
   else:
     sys.exit('Android supported only for Linux and OSX')
+
+if is_android:
+  url, sha1 = get_android_url()
+  FileToDownload(url, sha1, android_archive_local, ci_dir)
 
 if is_ninja:
   ninja = FileToDownload(
