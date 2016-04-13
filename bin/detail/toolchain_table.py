6ec82da11f5be007d85fb384cb401afcb28a824d
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-04-13 12:36:10 +0700

    Add 'osx-10-10-dep-10-9-make' toolchain

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -193,6 +193,7 @@ if platform.system() == 'Darwin':
       Toolchain('osx-10-10', 'Xcode', osx_version='10.10'),
       Toolchain('osx-10-11', 'Xcode', osx_version='10.11'),
       Toolchain('osx-10-10-dep-10-7', 'Xcode', osx_version='10.10'),
+      Toolchain('osx-10-10-dep-10-9-make', 'Unix Makefiles'),
       Toolchain('linux-gcc-x64', 'Unix Makefiles'),
   ]
 
