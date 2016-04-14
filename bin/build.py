2225a36f23ac2e81332cf475dca75b4aee4e9e96
Xpol Wan <xpolife@gmail.com>
2016-04-14 16:14:07 +0800

    Add build.py for backward compatibility.

---
+++ bin/build.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2014-2015, Ruslan Baratov
+# All rights reserved.
+
+from os.path import abspath, dirname, join
+exec(open(join(dirname(abspath(__file__)), 'polly.py'), 'r').read())
