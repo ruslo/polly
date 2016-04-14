aa16a8e812e7a72685a0f5af0c892b9fb2a44229
Xpol Wan <xpolife@gmail.com>
2016-04-14 16:21:48 +0800

    Add notes to build.py.

--- bin/build.py
+++ bin/build.py
@@ -3,5 +3,7 @@
 # Copyright (c) 2014-2015, Ruslan Baratov
 # All rights reserved.
 
+# Note: build.py has been renamed to polly.py.
+
 from os.path import abspath, dirname, join
 exec(open(join(dirname(abspath(__file__)), 'polly.py'), 'r').read())
