3c4b59aa1d8a6c4e98f0b86343ed10c2be1b5b3e
Michele Caini <michele.caini@gmail.com>
2016-04-18 12:30:50 +0200

    WIP: added android-ndx-r11c-*.cmake files

---
+++ android-ndk-r11c-api-21-mips.cmake
@@ -0,0 +1,29 @@
+# Copyright (c) 2016, Michele Caini
+# All rights reserved.
+
+if(DEFINED POLLY_ANDROID_NDK_R11C_API_21_MIPS_CMAKE_)
+  return()
+else()
+  set(POLLY_ANDROID_NDK_R11C_API_21_MIPS_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+set(ANDROID_NDK_VERSION "r11c")
+set(ANDROID_NATIVE_API_LEVEL "21")
+set(ANDROID_ABI "mips")
+
+polly_init(
+    "Android NDK ${ANDROID_NDK_VERSION} / \
+API ${ANDROID_NATIVE_API_LEVEL} / ${ANDROID_ABI} / \
+c++11 support"
+    "Unix Makefiles"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake") # before toolchain!
+
+include("${CMAKE_CURRENT_LIST_DIR}/os/android.cmake")
