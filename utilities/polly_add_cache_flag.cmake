c8914b47d5da5349779b42a982adfcba7c9e29f2
yowidin <yowidin@gmail.com>
2016-06-06 16:21:47 +0200

    polly_add_cache_flag multiple flags support
    Functionality is essentially the same but it is now a bit easier to
    provide multiple related flags at once.

--- utilities/polly_add_cache_flag.cmake
+++ utilities/polly_add_cache_flag.cmake
@@ -1,31 +1,23 @@
 # Copyright (c) 2014, Ruslan Baratov
 # All rights reserved.
 
-# Add flag to CACHE variable. Do nothing if flag already exists.
-#
-# Note:
-#   flags should be added one by one since this function check that
-#   substring "flag" already exists in string "var_name".
-#
-# Bad:
-#   polly_add_cache_flag(CMAKE_CXX_FLAGS "-opt1 -opt2 -opt3")
-#
-# Good:
-#   polly_add_cache_flag(CMAKE_CXX_FLAGS "-opt1")
-#   polly_add_cache_flag(CMAKE_CXX_FLAGS "-opt2")
-#   polly_add_cache_flag(CMAKE_CXX_FLAGS "-opt3")
-#
-function(polly_add_cache_flag var_name flag)
-  set(spaced_string " ${${var_name}} ")
-  string(FIND "${spaced_string}" " ${flag} " flag_index)
-  if(NOT flag_index EQUAL -1)
-    return()
-  endif()
-  string(COMPARE EQUAL "" "${${var_name}}" is_empty)
-  if(is_empty)
-    # beautify: avoid extra space at the end if var_name is empty
-    set("${var_name}" "${flag}" CACHE STRING "" FORCE)
-  else()
-    set("${var_name}" "${flag} ${${var_name}}" CACHE STRING "" FORCE)
-  endif()
+# Add one or more flags to CACHE variable. Skip already existing flags.
+function(polly_add_cache_flag var_name flags)
+  # Break flags string into CMake list
+  set(flags_list ${flags})
+  string(REGEX REPLACE "[ \n\t;]+" ";" flags_list ${flags_list})
+
+  foreach(flag ${flags_list})
+    # Check flag presense within the list
+    string(FIND "${${var_name}}" "${flag}" flag_index)
+    if(flag_index EQUAL -1)
+      string(LENGTH  "${${var_name}}" var_lng)
+      if(var_lng EQUAL 0)
+        # beautify: avoid extra space at the end if var_name is empty
+        set("${var_name}" "${flag}" CACHE STRING "" FORCE)
+      else()
+        set("${var_name}" "${flag} ${${var_name}}" CACHE STRING "" FORCE)
+      endif()
+    endif()
+  endforeach()
 endfunction()
