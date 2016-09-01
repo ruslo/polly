f905f2fbf8297876e8e047a9f0ba0eebb61d9ec8
Alexandre Pretyman <alexandre.pretyman@gmail.com>
2016-08-31 23:55:46 -0300

    Added emscripten-cxx11 toolchain

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -91,6 +91,7 @@ toolchain_table = [
     Toolchain('android-ndk-r11c-api-21-x86-64-hid', 'Unix Makefiles'),
     Toolchain('android-ndk-r11c-api-21-mips', 'Unix Makefiles'),
     Toolchain('android-ndk-r11c-api-21-mips64', 'Unix Makefiles'),
+    Toolchain('emscripten-cxx11', 'Unix Makefiles'),
     Toolchain('raspberrypi2-cxx11', 'Unix Makefiles')
 ]
 
