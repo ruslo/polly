2dd469e18ca4921d1bf26b55e70a90225b114561
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-09-28 13:52:57 +0300

    Add 'osx-10-11-sanitize-address' (Xcode) toolchain

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -292,6 +292,7 @@ if platform.system() == 'Darwin':
       Toolchain('osx-10-11', 'Xcode', osx_version='10.11'),
       Toolchain('osx-10-10-dep-10-7', 'Xcode', osx_version='10.10'),
       Toolchain('osx-10-10-dep-10-9-make', 'Unix Makefiles'),
+      Toolchain('osx-10-11-sanitize-address', 'Xcode', osx_version='10.11'),
       Toolchain('linux-gcc-x64', 'Unix Makefiles'),
   ]
 
