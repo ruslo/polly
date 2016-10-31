4d7dfb69b9e1ba3df19979f0ac5a3fc2ad49c718
Esteve Fernandez <esteve@apache.org>
2016-10-31 18:52:13 +0100

    Added support for iOS 10.1

---
+++ ios-nocodesign-10-1.cmake
@@ -0,0 +1,67 @@
+# Copyright (c) 2014-2016, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_IOS_NOCODESIGN_10_1_CMAKE_)
+  return()
+else()
+  set(POLLY_IOS_NOCODESIGN_10_1_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+set(IOS_SDK_VERSION 10.1)
+set(POLLY_XCODE_COMPILER "clang")
+polly_init(
+    "iOS ${IOS_SDK_VERSION} Universal (iphoneos + iphonesimulator) / \
+${POLLY_XCODE_COMPILER} / \
+No code sign / \
+c++11 support"
+    "Xcode"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include(polly_fatal_error)
+
+# Fix try_compile
+set(MACOSX_BUNDLE_GUI_IDENTIFIER com.example)
+set(CMAKE_MACOSX_BUNDLE YES)
+
+# Verify XCODE_XCCONFIG_FILE
+set(
+    _polly_xcode_xcconfig_file_path
+    "${CMAKE_CURRENT_LIST_DIR}/scripts/NoCodeSign.xcconfig"
+)
+if(NOT EXISTS "$ENV{XCODE_XCCONFIG_FILE}")
+  polly_fatal_error(
+      "Path specified by XCODE_XCCONFIG_FILE environment variable not found"
+      "($ENV{XCODE_XCCONFIG_FILE})"
+      "Use this command to set: "
+      "    export XCODE_XCCONFIG_FILE=${_polly_xcode_xcconfig_file_path}"
+  )
+else()
+  string(
+      COMPARE
+      NOTEQUAL
+      "$ENV{XCODE_XCCONFIG_FILE}"
+      "${_polly_xcode_xcconfig_file_path}"
+      _polly_wrong_xcconfig_path
+  )
+  if(_polly_wrong_xcconfig_path)
+    polly_fatal_error(
+        "Unexpected XCODE_XCCONFIG_FILE value: "
+        "    $ENV{XCODE_XCCONFIG_FILE}"
+        "expected: "
+        "    ${_polly_xcode_xcconfig_file_path}"
+    )
+  endif()
+endif()
+
+set(IPHONEOS_ARCHS armv7;armv7s;arm64)
+set(IPHONESIMULATOR_ARCHS i386;x86_64)
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/xcode.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/os/iphone.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake")
