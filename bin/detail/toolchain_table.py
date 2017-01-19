d27a8952f73409265f0c93bc2c893bc22af50f8b
Cristiano di Flora <oh3epx@gmail.com>
2017-01-19 23:09:01 +0200

    Add 'osx-10-12-ninja' toolchain

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -331,6 +331,7 @@ if platform.system() == 'Darwin':
       Toolchain('osx-10-10-dep-10-9-make', 'Unix Makefiles'),
       Toolchain('osx-10-11-make', 'Unix Makefiles'),
       Toolchain('osx-10-12-make', 'Unix Makefiles'),
+      Toolchain('osx-10-12-ninja', 'Ninja'),
       Toolchain('osx-10-11-sanitize-address', 'Xcode', osx_version='10.11'),
       Toolchain('osx-10-12-sanitize-address', 'Xcode', osx_version='10.12'),
       Toolchain('osx-10-12-sanitize-address-hid-sections', 'Xcode', osx_version='10.12'),
