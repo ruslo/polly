7c3d0cda9e56fe385b5de18368d7b2c43cb9b314
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-05-07 16:34:43 +0300

    Add 'gcc-pic-hid-sections-lto' toolchain
    [skip ci]

---
+++ gcc-pic-hid-sections-lto.cmake
@@ -0,0 +1,25 @@
+# Copyright (c) 2016-2017, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_GCC_PIC_HID_SECTIONS_LTO_CMAKE_)
+  return()
+else()
+  set(POLLY_GCC_PIC_HID_SECTIONS_LTO_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+polly_init(
+    "gcc / PIC / c++11 support / hidden / function-sections / data-sections / LTO"
+    "Unix Makefiles"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/fpic.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/function-sections.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/data-sections.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/hidden.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/lto.cmake")
