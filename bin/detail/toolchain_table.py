5211b4c562d97a57b314a64ef72e1a9de3600521
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-05-14 11:46:27 +0300

    Add 'gcc-5-pic-hid-sections-lto'
    [skip ci]

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -388,6 +388,7 @@ if os.name == 'posix':
       Toolchain('gcc-4-8-pic-hid-sections', 'Unix Makefiles'),
       Toolchain('gcc-pic-hid-sections', 'Unix Makefiles'),
       Toolchain('gcc-pic-hid-sections-lto', 'Unix Makefiles'),
+      Toolchain('gcc-5-pic-hid-sections-lto', 'Unix Makefiles'),
       Toolchain('gcc-cxx98', 'Unix Makefiles'),
       Toolchain('gcc-lto', 'Unix Makefiles'),
       Toolchain('libcxx', 'Unix Makefiles'),
