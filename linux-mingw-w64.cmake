8131bb72fcb6df9a69e1b0e4cb3d39a6b00b4ac0
Reinhold Gschweicher <rgschweicher@riegl.com>
2017-06-22 07:59:22 +0200

    mingw: move RC and Fortran to gcc-cross-compile

--- linux-mingw-w64.cmake
+++ linux-mingw-w64.cmake
@@ -20,15 +20,9 @@ include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 set(CMAKE_SYSTEM_NAME Windows)
 set(CROSS_COMPILE_TOOLCHAIN_PREFIX "x86_64-w64-mingw32")
 
-set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
-
 include(
     "${CMAKE_CURRENT_LIST_DIR}/compiler/gcc-cross-compile-simple-layout.cmake"
 )
 include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/static.cmake")
 
-# set fortran compiler (part of mingw-w64)
-set(CMAKE_Fortran_COMPILER "${TOOLCHAIN_PATH_AND_PREFIX}-gfortran" CACHE PATH "gfortran" )
-# set RC compiler
-set(CMAKE_RC_COMPILER    "${TOOLCHAIN_PATH_AND_PREFIX}-windres" CACHE PATH "windres" )
