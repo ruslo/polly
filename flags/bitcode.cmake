ce4d496fc6f0d516c41b88e95ffc5b8f187fe487
Ihar Kliashchou <ihar.kliashchou@gmail.com>
2017-07-17 16:38:47 +0300

    correct bitcode support for iOS updated (toolchain ID 3) debug marker mode added comments added

--- flags/bitcode.cmake
+++ flags/bitcode.cmake
@@ -7,12 +7,16 @@ else()
   set(POLLY_FLAGS_BITCODE_ 1)
 endif()
 
-include(polly_add_cache_flag)
+set(CMAKE_XCODE_ATTRIBUTE_ENABLE_BITCODE YES)
 
-polly_add_cache_flag(CMAKE_CXX_FLAGS "-fembed-bitcode")
+# It is not enough to set ENABLE_BITCODE to get binaries built with real bitcode information inside 
+# We should set BITCODE_GENERATION_MODE to "bitcode" for release builds and to "marker" for debug builds 
+# Only release builds usually need real bitcode for submission to Apple AppStore, so we'll save time on build
+#
+# https://medium.com/@heitorburger/static-libraries-frameworks-and-bitcode-6d8f784478a9
 
-# We can't use 'ENABLE_BITCODE' because
-# it only adds '-fembed-bitcode-marker':
-# * https://stackoverflow.com/a/31346742
+set(CMAKE_XCODE_ATTRIBUTE_BITCODE_GENERATION_MODE[variant=Debug] "marker")
+set(CMAKE_XCODE_ATTRIBUTE_BITCODE_GENERATION_MODE "bitcode")
 
-list(APPEND HUNTER_TOOLCHAIN_UNDETECTABLE_ID "bitcode.2")
+list(APPEND HUNTER_TOOLCHAIN_UNDETECTABLE_ID "bitcode.3")
+
