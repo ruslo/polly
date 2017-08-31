f45b6f56a9251fc54d37ffbfaa7edfd24dbde6a6
Tomas Zemaitis <t.zemaitis@idscan.co.uk>
2017-08-31 22:57:26 +0300

    Added intel toolchains for r15c NDK

---
+++ android-ndk-r15c-api-19-x86.cmake
@@ -0,0 +1,29 @@
+# Copyright (c) 2016, Michele Caini
+# All rights reserved.
+
+if(DEFINED POLLY_ANDROID_NDK_R15C_API_19_X86_CMAKE_)
+  return()
+else()
+  set(POLLY_ANDROID_NDK_R15C_API_19_X86_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+set(ANDROID_NDK_VERSION "r15c")
+set(CMAKE_SYSTEM_VERSION "19")
+set(CMAKE_ANDROID_ARCH_ABI "x86")
+
+polly_init(
+    "Android NDK ${ANDROID_NDK_VERSION} / \
+API ${CMAKE_SYSTEM_VERSION} / ${CMAKE_ANDROID_ARCH_ABI} / \
+c++11 support"
+    "Unix Makefiles"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake") # before toolchain!
+
+include("${CMAKE_CURRENT_LIST_DIR}/os/android.cmake")
