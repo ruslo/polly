d89778876b3bc5f538c13baebd58d05cecd5be70
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-09-08 14:54:37 +0300

    Add 'ios-nocodesign-10-3-dep-9-0-bitcode' toolchain
    [skip ci]

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -367,6 +367,7 @@ if platform.system() == 'Darwin':
       Toolchain('ios-nocodesign-10-1-dep-9-0-device-libcxx-hid-sections-lto', 'Xcode', ios_version='10.1', nocodesign=True),
       Toolchain('ios-nocodesign-10-2', 'Xcode', ios_version='10.2', nocodesign=True),
       Toolchain('ios-nocodesign-10-3', 'Xcode', ios_version='10.3', nocodesign=True),
+      Toolchain('ios-nocodesign-10-3-dep-9-0-bitcode', 'Xcode', ios_version='10.3', nocodesign=True),
       Toolchain('xcode', 'Xcode'),
       Toolchain('xcode-cxx98', 'Xcode'),
       Toolchain('xcode-nocxx', 'Xcode'),
