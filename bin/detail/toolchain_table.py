1d55ebb9531fff7b0a86d42d38c60fbe3b04f971
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-09-13 16:03:44 +0300

    Add 'android-ndk-r15c-api-21-mips-clang-libcxx' toolchain
    [skip ci]

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -117,6 +117,7 @@ toolchain_table = [
     Toolchain('android-ndk-r14-api-21-x86-64', 'Unix Makefiles'),
     Toolchain('android-ndk-r15c-api-21-armeabi-v7a-neon-clang-libcxx', 'Unix Makefiles'),
     Toolchain('android-ndk-r15c-api-21-armeabi-clang-libcxx', 'Unix Makefiles'),
+    Toolchain('android-ndk-r15c-api-21-mips-clang-libcxx', 'Unix Makefiles'),
     Toolchain('emscripten-cxx11', 'Unix Makefiles'),
     Toolchain('raspberrypi2-cxx11', 'Unix Makefiles'),
     Toolchain('raspberrypi2-cxx11-pic', 'Unix Makefiles'),
