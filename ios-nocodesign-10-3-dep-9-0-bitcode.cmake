806d74c88fb8e75069d59633e33d3fd021e3c334
Adam <aturkelson@gmail.com>
2017-09-18 16:40:16 -0400

    Update ios-nocodesign-10-3-dep-9-0-bitcode.cmake
    nocodesign should not invoke the IOS development team dependency

--- ios-nocodesign-10-3-dep-9-0-bitcode.cmake
+++ ios-nocodesign-10-3-dep-9-0-bitcode.cmake
@@ -70,5 +70,3 @@ include("${CMAKE_CURRENT_LIST_DIR}/compiler/xcode.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/os/iphone.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx11.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/bitcode.cmake") # after os/iphone.cmake
-
-include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_ios_development_team.cmake")
