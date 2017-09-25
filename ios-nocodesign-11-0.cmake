f9321a1b675f8bd2622738b92867fc00a54fee5e
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-09-25 21:17:38 +0300

    Cleanup
    [skip ci]

--- ios-nocodesign-11-0.cmake
+++ ios-nocodesign-11-0.cmake
@@ -2,9 +2,9 @@
 # All rights reserved.
 
 if(DEFINED POLLY_IOS_NOCODESIGN_11_0_CMAKE_)
-return()
+  return()
 else()
-set(POLLY_IOS_NOCODESIGN_11_0_CMAKE_ 1)
+  set(POLLY_IOS_NOCODESIGN_11_0_CMAKE_ 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
@@ -14,11 +14,11 @@ include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 set(IOS_SDK_VERSION 11.0)
 set(POLLY_XCODE_COMPILER "clang")
 polly_init(
-  "iOS ${IOS_SDK_VERSION} Universal (iphoneos + iphonesimulator) / \
+    "iOS ${IOS_SDK_VERSION} Universal (iphoneos + iphonesimulator) / \
 ${POLLY_XCODE_COMPILER} / \
 No code sign / \
 c++14 support"
-  "Xcode"
+    "Xcode"
 )
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
@@ -31,32 +31,32 @@ set(CMAKE_MACOSX_BUNDLE YES)
 
 # Verify XCODE_XCCONFIG_FILE
 set(
-  _polly_xcode_xcconfig_file_path
-  "${CMAKE_CURRENT_LIST_DIR}/scripts/NoCodeSign.xcconfig"
+    _polly_xcode_xcconfig_file_path
+    "${CMAKE_CURRENT_LIST_DIR}/scripts/NoCodeSign.xcconfig"
 )
 if(NOT EXISTS "$ENV{XCODE_XCCONFIG_FILE}")
-polly_fatal_error(
-    "Path specified by XCODE_XCCONFIG_FILE environment variable not found"
-    "($ENV{XCODE_XCCONFIG_FILE})"
-    "Use this command to set: "
-    "    export XCODE_XCCONFIG_FILE=${_polly_xcode_xcconfig_file_path}"
-)
-else()
-string(
-    COMPARE
-    NOTEQUAL
-    "$ENV{XCODE_XCCONFIG_FILE}"
-    "${_polly_xcode_xcconfig_file_path}"
-    _polly_wrong_xcconfig_path
-)
-if(_polly_wrong_xcconfig_path)
   polly_fatal_error(
-      "Unexpected XCODE_XCCONFIG_FILE value: "
-      "    $ENV{XCODE_XCCONFIG_FILE}"
-      "expected: "
-      "    ${_polly_xcode_xcconfig_file_path}"
+      "Path specified by XCODE_XCCONFIG_FILE environment variable not found"
+      "($ENV{XCODE_XCCONFIG_FILE})"
+      "Use this command to set: "
+      "    export XCODE_XCCONFIG_FILE=${_polly_xcode_xcconfig_file_path}"
   )
-endif()
+else()
+  string(
+      COMPARE
+      NOTEQUAL
+      "$ENV{XCODE_XCCONFIG_FILE}"
+      "${_polly_xcode_xcconfig_file_path}"
+      _polly_wrong_xcconfig_path
+  )
+  if(_polly_wrong_xcconfig_path)
+    polly_fatal_error(
+        "Unexpected XCODE_XCCONFIG_FILE value: "
+        "    $ENV{XCODE_XCCONFIG_FILE}"
+        "expected: "
+        "    ${_polly_xcode_xcconfig_file_path}"
+    )
+  endif()
 endif()
 
 set(IPHONEOS_ARCHS arm64)
