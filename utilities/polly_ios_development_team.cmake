5cc90f44341209c104e8b7fbb4233d1668707f84
Pascal Thomet <pthomet@ivsweb.com>
2017-09-25 16:41:12 +0200

    POLLY_IOS_BUNDLE_IDENTIFIER and POLLY_IOS_DEVELOPMENT_TEAM can be set  either through an environment variable or through a regular cmake variable
     This enables to create a wrapper toolchain that includes these variables i.e for example:
    
    ````
    set(POLLY_IOS_DEVELOPMENT_TEAM "XXXXXX")
    set(POLLY_IOS_BUNDLE_IDENTIFIER "com.example.pollyenabler")
    include("${CMAKE_CURRENT_LIST_DIR}/polly-toolchains/ios-11-0.cmake")
    `````

--- utilities/polly_ios_development_team.cmake
+++ utilities/polly_ios_development_team.cmake
@@ -10,18 +10,21 @@ endif()
 include("${CMAKE_CURRENT_LIST_DIR}/polly_fatal_error.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/polly_status_debug.cmake")
 
-string(COMPARE EQUAL "$ENV{POLLY_IOS_DEVELOPMENT_TEAM}" "" _is_empty)
-if(_is_empty)
-  polly_fatal_error(
-      "Environment variable POLLY_IOS_DEVELOPMENT_TEAM is empty"
-      " (see details: http://polly.readthedocs.io/en/latest/toolchains/ios/errors/polly_ios_development_team.html)"
-  )
+# POLLY_IOS_DEVELOPMENT_TEAM can be set either through an environment variable 
+# or through a regular cmake variable
+if (POLLY_IOS_DEVELOPMENT_TEAM)
+  set(CMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM "${POLLY_IOS_DEVELOPMENT_TEAM}")
+else()
+  string(COMPARE EQUAL "$ENV{POLLY_IOS_DEVELOPMENT_TEAM}" "" _is_empty)
+  if(_is_empty)
+    polly_fatal_error(
+        "Environment variable POLLY_IOS_DEVELOPMENT_TEAM is empty"
+        " (see details: http://polly.readthedocs.io/en/latest/toolchains/ios/errors/polly_ios_development_team.html)"
+    )
+  endif()
+  set(CMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM "$ENV{POLLY_IOS_DEVELOPMENT_TEAM}")
 endif()
 
-set(
-    CMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM
-    "$ENV{POLLY_IOS_DEVELOPMENT_TEAM}"
-)
 
 polly_status_debug(
     "Using iOS development team id: ${CMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM}"
