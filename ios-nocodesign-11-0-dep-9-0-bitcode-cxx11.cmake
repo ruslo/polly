aefd8964ed4f1a6313e62065180890e97e825ee3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-09-28 13:32:45 +0300

    Add 'ios-nocodesign-11-0-dep-9-0-bitcode-cxx11' toolchain
    [skip ci]

--- ios-nocodesign-11-0-dep-9-0-bitcode-cxx11.cmake
+++ ios-nocodesign-11-0-dep-9-0-bitcode-cxx11.cmake
@@ -1,17 +1,17 @@
-# Copyright (c) 2014-2017, Ruslan Baratov
+# Copyright (c) 2017, Ruslan Baratov
 # All rights reserved.
 
-if(DEFINED POLLY_IOS_NOCODESIGN_10_3_DEP_9_0_BITCODE_CMAKE_)
+if(DEFINED POLLY_IOS_NOCODESIGN_11_0_DEP_9_0_BITCODE_CMAKE_)
   return()
 else()
-  set(POLLY_IOS_NOCODESIGN_10_3_DEP_9_0_BITCODE_CMAKE_ 1)
+  set(POLLY_IOS_NOCODESIGN_11_0_DEP_9_0_BITCODE_CMAKE_ 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
-set(IOS_SDK_VERSION 10.3)
+set(IOS_SDK_VERSION 11.0)
 set(IOS_DEPLOYMENT_SDK_VERSION 9.0)
 
 set(POLLY_XCODE_COMPILER "clang")
