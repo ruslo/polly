a9d58114d3c8bfffe33128e6bc189d6a21bc3502
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-10-18 22:11:18 +0300

    Add 'ios-nocodesign-11-0-arm64-dep-9-0-device-libcxx-hid-sections'
    [skip ci]

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -389,6 +389,7 @@ if platform.system() == 'Darwin':
       Toolchain('ios-nocodesign-10-3-dep-9-0-bitcode', 'Xcode', ios_version='10.3', nocodesign=True),
       Toolchain('ios-nocodesign-11-0', 'Xcode', ios_version='11.0', nocodesign=True),
       Toolchain('ios-nocodesign-11-0-dep-9-0-bitcode-cxx11', 'Xcode', ios_version='11.0', nocodesign=True),
+      Toolchain('ios-nocodesign-11-0-arm64-dep-9-0-device-libcxx-hid-sections', 'Xcode', ios_version='11.0', nocodesign=True),
       Toolchain('xcode', 'Xcode'),
       Toolchain('xcode-cxx98', 'Xcode'),
       Toolchain('xcode-nocxx', 'Xcode'),
