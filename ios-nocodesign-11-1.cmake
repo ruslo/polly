dd2b50026d88f552707d715476e2deb63de468df
Clemens Arth <arth@Clemenss-MacBook-Pro.local>
2017-12-04 20:01:35 +0100

    update to most recent

---
+++ ios-nocodesign-11-1.cmake
@@ -0,0 +1,67 @@
+# Copyright (c) 2014-2016, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_IOS_NOCODESIGN_11_1_CMAKE_)
+  return()
+else()
+  set(POLLY_IOS_NOCODESIGN_11_1_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+set(IOS_SDK_VERSION 11.1)
+set(POLLY_XCODE_COMPILER "clang")
+polly_init(
+    "iOS ${IOS_SDK_VERSION} Universal (iphoneos + iphonesimulator) / \
+${POLLY_XCODE_COMPILER} / \
+No code sign / \
+c++14 support"
+    "Xcode"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include(polly_fatal_error)
+
+# Fix try_compile
+include(polly_ios_bundle_identifier)
+set(CMAKE_MACOSX_BUNDLE YES)
+
+# Verify XCODE_XCCONFIG_FILE
+set(
+    _polly_xcode_xcconfig_file_path
+    "${CMAKE_CURRENT_LIST_DIR}/scripts/NoCodeSign.xcconfig"
+)
+if(NOT EXISTS "$ENV{XCODE_XCCONFIG_FILE}")
+  polly_fatal_error(
+      "Path specified by XCODE_XCCONFIG_FILE environment variable not found"
+      "($ENV{XCODE_XCCONFIG_FILE})"
+      "Use this command to set: "
+      "    export XCODE_XCCONFIG_FILE=${_polly_xcode_xcconfig_file_path}"
+  )
+else()
+  string(
+      COMPARE
+      NOTEQUAL
+      "$ENV{XCODE_XCCONFIG_FILE}"
+      "${_polly_xcode_xcconfig_file_path}"
+      _polly_wrong_xcconfig_path
+  )
+  if(_polly_wrong_xcconfig_path)
+    polly_fatal_error(
+        "Unexpected XCODE_XCCONFIG_FILE value: "
+        "    $ENV{XCODE_XCCONFIG_FILE}"
+        "expected: "
+        "    ${_polly_xcode_xcconfig_file_path}"
+    )
+  endif()
+endif()
+
+set(IPHONEOS_ARCHS arm64)
+set(IPHONESIMULATOR_ARCHS x86_64)
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/xcode.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/os/iphone.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx14.cmake")
