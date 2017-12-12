c45cff8954f428bbbdc1fdf573e830ae64783ae2
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-12-12 11:08:04 +0300

    Add 'ios-11-2-dep-9-0-device-bitcode-cxx11' toolchain

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -292,6 +292,7 @@ if platform.system() == 'Linux':
 if platform.system() == 'Darwin':
   toolchain_table += [
       Toolchain('ios', 'Xcode'),
+      Toolchain('ios-11-2-dep-9-0-device-bitcode-cxx11', 'Xcode', ios_version='11.2'),
       Toolchain('ios-11-1-dep-9-0-bitcode-cxx11', 'Xcode', ios_version='11.1'),
       Toolchain('ios-11-1-dep-9-0-device-bitcode-cxx11', 'Xcode', ios_version='11.1'),
       Toolchain('ios-11-0-dep-9-0-bitcode-cxx11', 'Xcode', ios_version='11.0'),
