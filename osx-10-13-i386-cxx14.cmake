1a54872739c0ac9b042f69ca3ac687acc6d14302
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-03-24 18:33:20 +0300

    Add 'osx-10-13-i386-cxx14' toolchain
    [skip ci]

---
+++ osx-10-13-i386-cxx14.cmake
@@ -0,0 +1,32 @@
+# Copyright (c) 2016, 2018 Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_OSX_10_13_CXX14_CMAKE_)
+  return()
+else()
+  set(POLLY_OSX_10_13_CXX14_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+set(OSX_SDK_VERSION "10.13")
+set(POLLY_XCODE_COMPILER "clang")
+
+set(CMAKE_OSX_ARCHITECTURES i386 CACHE INTERNAL "")
+
+polly_init(
+    "Xcode (OS X ${OSX_SDK_VERSION}) / \
+${POLLY_XCODE_COMPILER} / ${CMAKE_OSX_ARCHITECTURES} \
+LLVM Standard C++ Library (libc++) / c++14 support"
+    "Xcode"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/xcode.cmake")
+
+set(CMAKE_OSX_DEPLOYMENT_TARGET "10.13" CACHE STRING "OS X Deployment target" FORCE)
+
+include("${CMAKE_CURRENT_LIST_DIR}/library/std/libcxx.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx14.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/os/osx.cmake")
