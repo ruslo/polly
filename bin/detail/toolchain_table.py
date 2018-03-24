5b5e904a673d470d94c44567c54612348e40566b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-03-24 17:17:27 +0300

    Add 'vs-15-2017-win64-store-10-zw' toolchain
    [skip ci]

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -303,6 +303,12 @@ if os.name == 'nt':
           vs_version='15'
       ),
       Toolchain(
+          'vs-15-2017-win64-store-10-zw',
+          'Visual Studio 15 2017 Win64',
+          arch='amd64',
+          vs_version='15'
+      ),
+      Toolchain(
           'android-vc-ndk-r10e-api-19-arm-clang-3-6',
           'Visual Studio 14 2015 ARM',
           arch='',
