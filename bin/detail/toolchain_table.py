4f4ec10c9d472290320bec1a55466014e391d7c8
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-03-25 18:11:49 +0300

    Add 'vs-15-2017-store-10-zw' toolchain

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -310,6 +310,12 @@ if os.name == 'nt':
           vs_version='15'
       ),
       Toolchain(
+          'vs-15-2017-store-10-zw',
+          'Visual Studio 15 2017',
+          arch='x86',
+          vs_version='15'
+      ),
+      Toolchain(
           'android-vc-ndk-r10e-api-19-arm-clang-3-6',
           'Visual Studio 14 2015 ARM',
           arch='',
