97bd8e3e1e83529f6001b6741338b9e2412f6f41
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-04-11 18:31:28 +0300

    Add ios-nocodesign-11-2-dep-9-3-i386-armv7 toolchain
    [skip ci]

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -485,6 +485,7 @@ if platform.system() == 'Darwin':
       Toolchain('ios-nocodesign-11-2-dep-9-3-armv7', 'Xcode', ios_version='11.2', nocodesign=True),
       Toolchain('ios-nocodesign-11-2-dep-9-3-arm64', 'Xcode', ios_version='11.2', nocodesign=True),
       Toolchain('ios-nocodesign-11-2-dep-9-3-arm64-armv7', 'Xcode', ios_version='11.2', nocodesign=True),
+      Toolchain('ios-nocodesign-11-2-dep-9-3-i386-armv7', 'Xcode', ios_version='11.2', nocodesign=True),
       Toolchain('ios-nocodesign-11-2', 'Xcode', ios_version='11.2', nocodesign=True),
       Toolchain('xcode', 'Xcode'),
       Toolchain('xcode-cxx98', 'Xcode'),
