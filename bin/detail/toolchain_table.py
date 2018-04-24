4a9282d81a946256f8a573f1ae21c3cdf05fb925
Shun Kei Leung <kevinlsk@gmail.com>
2018-04-24 13:13:44 +0800

    Add ios-11-3-dep-9-0-device-bitcode-cxx11 toolchain

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -373,6 +373,7 @@ if platform.system() == 'Linux':
 if platform.system() == 'Darwin':
   toolchain_table += [
       Toolchain('ios', 'Xcode'),
+      Toolchain('ios-11-3-dep-9-0-device-bitcode-cxx11', 'Xcode', ios_version='11.3'),
       Toolchain('ios-11-2-dep-9-0-device-bitcode-cxx11', 'Xcode', ios_version='11.2'),
       Toolchain('ios-11-2-dep-9-3-arm64-armv7', 'Xcode', ios_version='11.2'),
       Toolchain('ios-11-3-dep-9-3-arm64-armv7', 'Xcode', ios_version='11.3'),
