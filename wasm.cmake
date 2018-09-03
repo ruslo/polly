b73f60d14c6952a7eec347563834a5edd47fb1b1
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2018-09-04 00:18:19 +0200

    :wrench: clearly states the system is a linux, this brings CMake to search cmake package configs for library in the linux way.
    See the following table taken from : https://cmake.org/cmake/help/latest/command/find_package.html
    
    nxxm relies on find_package(XXX CONFIG REQUIRED) mode, platform_deps like boost are installed via Hunter.
    Hunter somehow installs BoostConfig.cmake stuff under a **UNIX (U)** convention, but if SYSTEM_NAME is
    Emscripten as CMake doesn't know how to search for package configs in this mode it ends up searching as he
    would on a windows.
    
    Each entry is meant for installation trees following Windows (W), UNIX (U), or Apple (A) conventions:
    
    <prefix>/                                                       (W)
    <prefix>/(cmake|CMake)/                                         (W)
    <prefix>/<name>*/                                               (W)
    <prefix>/<name>*/(cmake|CMake)/                                 (W)
    <prefix>/(lib/<arch>|lib*|share)/cmake/<name>*/                 (U)
    <prefix>/(lib/<arch>|lib*|share)/<name>*/                       (U)
    <prefix>/(lib/<arch>|lib*|share)/<name>*/(cmake|CMake)/         (U)
    <prefix>/<name>*/(lib/<arch>|lib*|share)/cmake/<name>*/         (W/U)
    <prefix>/<name>*/(lib/<arch>|lib*|share)/<name>*/               (W/U)
    <prefix>/<name>*/(lib/<arch>|lib*|share)/<name>*/(cmake|CMake)/ (W/U)

--- wasm.cmake
+++ wasm.cmake
@@ -19,7 +19,7 @@ cmake_minimum_required(VERSION 3.0.0)
 # After that, build the generated Makefile with the command 'make'. On Windows, you may download and use 'mingw32-make' instead.
 
 # The following variable describes the target OS we are building to.
-set(CMAKE_SYSTEM_NAME Emscripten)
+set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 
 set(CMAKE_CROSSCOMPILING TRUE)
