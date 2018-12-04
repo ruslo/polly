2d543bf49b6180b3b84ba523a8488d8e05464f9d
David Hirvonen <dhirvonen@elucideye.com>
2018-12-04 14:07:25 -0500

    add android-ndk-r18b-api-24-arm64-v8a-clang-libcxx11

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -166,6 +166,7 @@ toolchain_table = [
     Toolchain('android-ndk-r17-api-19-armeabi-v7a-neon-hid-sections', 'Unix Makefiles'),
     Toolchain('android-ndk-r17-api-19-armeabi-v7a-neon-clang-libcxx', 'Unix Makefiles'),
     Toolchain('android-ndk-r18-api-24-arm64-v8a-clang-libcxx14', 'Unix Makefiles'),
+    Toolchain('android-ndk-r18b-api-24-arm64-v8a-clang-libcxx11', 'Unix Makefiles'),
     Toolchain('emscripten-cxx11', 'Unix Makefiles'),
     Toolchain('emscripten-cxx14', 'Unix Makefiles'),
     Toolchain('emscripten-cxx17', 'Unix Makefiles'),
