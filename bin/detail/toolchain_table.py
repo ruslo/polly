9525fba435775fabb45ae7302212894593c8d9cf
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-01-09 15:44:46 +0300

    Add 'android-ndk-r18b-api-21-armeabi-v7a-clang-libcxx' toolchain

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -170,6 +170,7 @@ toolchain_table = [
     Toolchain('android-ndk-r18b-api-24-arm64-v8a-clang-libcxx11', 'Unix Makefiles'),
     Toolchain('android-ndk-r18b-api-16-armeabi-v7a-clang-libcxx', 'Unix Makefiles'),
     Toolchain('android-ndk-r18b-api-21-arm64-v8a-clang-libcxx', 'Unix Makefiles'),
+    Toolchain('android-ndk-r18b-api-21-armeabi-v7a-clang-libcxx', 'Unix Makefiles'),
     Toolchain('emscripten-cxx11', 'Unix Makefiles'),
     Toolchain('emscripten-cxx14', 'Unix Makefiles'),
     Toolchain('emscripten-cxx17', 'Unix Makefiles'),
