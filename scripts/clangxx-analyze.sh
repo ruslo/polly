416ec461ea6d50d359946d72eef65607b2fed4c7
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-03-15 13:05:30 +0300

    Clang analyze: Get rid of the '-v' flag

--- scripts/clangxx-analyze.sh
+++ scripts/clangxx-analyze.sh
@@ -12,12 +12,23 @@ do
     temp_out="`mktemp /tmp/polly-clang-analyze.out.XXXXX`"
     temp_bin="`mktemp /tmp/polly-clang-analyze.bin.XXXXX`"
 
+    # Get rid of the '-v' flag (verbose)
+    all_options="$@"
+    if [ "${all_options}" == "-v" ];
+    then
+      all_options=""
+    fi
+
+    all_options=`echo "${all_options}" | sed 's,^-v ,,g'`
+    all_options=`echo "${all_options}" | sed 's, -v$,,g'`
+    all_options=`echo "${all_options}" | sed 's, -v , ,g'`
+
     # analyze
     # -w : ignore regular compiler warnings so 'temp_out' only contains
     # messages from analyzer. '-w' should not be a part of toolchain flags
     # since Hunter toolchain-id calculated using '#pragma message' output which
     # is implemented as a warning message (hence will be suppressed by '-w')
-    clang++ --analyze -w "$@" -o "${temp_bin}" 2> "${temp_out}"
+    clang++ --analyze -w ${all_options} -o "${temp_bin}" 2> "${temp_out}"
 
     RESULT=0
     [ "$?" == 0 ] || RESULT=1
