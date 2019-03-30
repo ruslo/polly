5c08df90d73b471f736abedcd54aaa485aaac48e
dvirtz <dvirtz@gmail.com>
2019-03-31 00:31:14 +0300

    add -fconcepts support

---
+++ ninja-gcc-7-cxx17-fconcepts.cmake
@@ -0,0 +1,22 @@
+# Copyright (c) 2016-2017, Ruslan Baratov
+# Copyright (c) 2017, David Hirvonen
+# All rights reserved.
+
+if(DEFINED POLLY_GCC_7_CXX17_CONCEPTS_CMAKE_)
+  return()
+else()
+  set(POLLY_GCC_7_CXX17_CONCEPTS_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+polly_init(
+    "gcc 7 / c++17 support / Concepts"
+    "Ninja"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc-7.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/fconcepts.cmake")
