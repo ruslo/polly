6ef4a3cb29e40bf2ed5fc67b58c70279f5fb71ae
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-04-26 01:57:15 +0300

    Set CMAKE_C_STANDARD variables

--- flags/c11.cmake
+++ flags/c11.cmake
@@ -16,3 +16,10 @@ polly_add_cache_flag(CMAKE_C_FLAGS_INIT "-std=c11")
 
 # Hunter doesn't run toolchain-id calculation for C compiler
 list(APPEND HUNTER_TOOLCHAIN_UNDETECTABLE_ID "c11")
+
+# Set CMAKE_C_STANDARD to cache to override project local value if present.
+# FORCE added in case CMAKE_C_STANDARD already set in cache
+# (e.g. set before 'project' by user).
+set(CMAKE_C_STANDARD 11 CACHE STRING "C Standard (toolchain)" FORCE)
+set(CMAKE_C_STANDARD_REQUIRED YES CACHE BOOL "C Standard required" FORCE)
+set(CMAKE_C_EXTENSIONS NO CACHE BOOL "C Standard extensions" FORCE)
