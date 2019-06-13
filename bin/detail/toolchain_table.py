054e75c7c56079bf3ca39f73c940e8d9d1e904a2
arrtchiu <arrtchiu@gmail.com>
2019-06-13 14:55:58 +0800

    add vs-15-2017-{,win64-}cxx14-mt

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -278,6 +278,9 @@ if os.name == 'nt':
           'vs-15-2017-mt', 'Visual Studio 15 2017', arch='x86', vs_version='15'
       ),
       Toolchain(
+          'vs-15-2017-cxx14-mt', 'Visual Studio 15 2017', arch='x86', vs_version='15'
+      ),
+      Toolchain(
           'vs-15-2017-cxx17', 'Visual Studio 15 2017', arch='x86', vs_version='15'
       ),
       Toolchain(
@@ -343,6 +346,12 @@ if os.name == 'nt':
       ),
       Toolchain(
           'vs-15-2017-win64-mt',
+          'Visual Studio 15 2017 Win64',
+          arch='amd64',
+          vs_version='15'
+      ),
+      Toolchain(
+          'vs-15-2017-win64-cxx14-mt',
           'Visual Studio 15 2017 Win64',
           arch='amd64',
           vs_version='15'
