0798587113f3592961e1e776a99c33348c46ad33
Boris Rasin <boris@pointx.org>
2019-07-03 09:34:42 +0300

    update Emscripten environment variable and cmake path

--- compiler/emscripten.cmake
+++ compiler/emscripten.cmake
@@ -9,13 +9,12 @@ endif()
 
 include(polly_fatal_error)
 
-string(COMPARE EQUAL "$ENV{EMSCRIPTEN}" "" _is_empty)
+string(COMPARE EQUAL "$ENV{EMSDK}" "" _is_empty)
 if(_is_empty)
   polly_fatal_error(
-    "EMSCRIPTEN environment variable not set. Emscripten environment variables are in emsdk_env.sh"
+    "EMSDK environment variable not set. Emscripten environment variables are in emsdk_env.sh"
   )
 endif()
 
-include("$ENV{EMSCRIPTEN}/cmake/Modules/Platform/Emscripten.cmake")
+include("$ENV{EMSDK}/fastcomp/emscripten/cmake/Modules/Platform/Emscripten.cmake")
 list(APPEND CMAKE_FIND_ROOT_PATH "${CMAKE_CURRENT_LIST_DIR}/emscripten")
-
