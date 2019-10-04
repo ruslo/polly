fd2eb02d23141199d1940d1dc1d98315faaff494
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-10-04 09:23:38 +0300

    Add iOS 13.0 toolchains

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -509,6 +509,8 @@ if platform.system() == 'Darwin':
   toolchain_table += [
       Toolchain('ios', 'Xcode'),
       Toolchain('ios-bitcode', 'Xcode'),
+      Toolchain('ios-13-0-dep-9-3-arm64', 'Xcode', ios_version='13.0'),
+      Toolchain('ios-13-0-dep-9-3-arm64-bitcode', 'Xcode', ios_version='13.0'),
       Toolchain('ios-12-3-dep-9-3-arm64', 'Xcode', ios_version='12.3'),
       Toolchain('ios-12-2-dep-9-3-arm64', 'Xcode', ios_version='12.2'),
       Toolchain('ios-12-1-dep-9-0-device-bitcode-cxx14', 'Xcode', ios_version='12.1'),
