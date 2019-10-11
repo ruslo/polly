e1ec5cd5a4787054e04e85cee2fcd250c137e3b5
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-10-11 17:37:35 +0300

    Add 'raspberrypi3-cxx14'

---
+++ raspberrypi3-cxx14.cmake
@@ -0,0 +1,24 @@
+# Copyright (c) 2015, 2017 Alexandre Pretyman
+# All rights reserved.
+
+if(DEFINED POLLY_RASPBERRYPI3_CXX14_CMAKE)
+  return()
+else()
+  set(POLLY_RASPBERRYPI3_CXX14_CMAKE 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+polly_init(
+    "RaspberryPi 3 Cross Compile / C++14"
+    "Unix Makefiles"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include(polly_clear_environment_variables)
+
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx14.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc-cross-compile-raspberry-pi.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/os/raspberry-pi3.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/os/raspberry-pi-hardfloat.cmake")
