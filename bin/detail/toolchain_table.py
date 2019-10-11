85b7564bc32a629341657c1c395c73dcd1309dde
Ruslan Baratov <ruslan_baratov@yahoo.com>
2019-10-11 18:44:24 +0300

    Add 'raspberrypi3-clang-cxx14' toolchain

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -184,6 +184,7 @@ toolchain_table = [
     Toolchain('raspberrypi2-cxx11', 'Unix Makefiles'),
     Toolchain('raspberrypi2-cxx11-pic', 'Unix Makefiles'),
     Toolchain('raspberrypi3-clang-cxx11', 'Unix Makefiles'),
+    Toolchain('raspberrypi3-clang-cxx14', 'Unix Makefiles'),
     Toolchain('raspberrypi3-gcc-pic-hid-sections', 'Unix Makefiles'),
     Toolchain('raspberrypi3-cxx14', 'Unix Makefiles'),
     Toolchain('raspberrypi3-cxx11', 'Unix Makefiles')
