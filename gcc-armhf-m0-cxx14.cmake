6c398213468b55ecc4baf666cc4ec9575c0563e0
Dylan Marcus <dylan@mach1studios.com>
2021-04-27 13:01:09 -0400

    cleanup for m0 arm toolchain

--- gcc-armhf-m0-cxx14.cmake
+++ gcc-armhf-m0-cxx14.cmake
@@ -16,13 +16,20 @@ polly_init(
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 
 # set system name, this sets the variable CMAKE_CROSSCOMPILING
-set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
 set(CROSS_COMPILE_TOOLCHAIN_PREFIX "arm-none-eabi")
+set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 include(
-    "${CMAKE_CURRENT_LIST_DIR}/compiler/gcc-cross-compile.cmake"
+    "${CMAKE_CURRENT_LIST_DIR}/compiler/gcc-cross-compile-simple-layout.cmake"
 )
 include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx14.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/hardfloat.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/flags/cortex-m0.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/flags/arm-none-eabi.cmake")
